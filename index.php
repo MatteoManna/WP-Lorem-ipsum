8bf51b53567c0b1aff45aa15ab0930acc2adf3a5
Matteo Manna <matteomanna87@gmail.com>
2017-03-24 08:52:47 +0100

    Init project
    Create new fake posts to fill the WordPress database.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP Lorem Ipsum
+Description: Automatically create new fake posts to fill the database and get a very good impression for your website.
+Author: Matteo Manna
+Version: 1.0
+Author URI: http://matteomanna.com/
+Text Domain: wp-lorem-ipsum
+License: GPL2
+*/
