34800f7a7ef6c1dbad7ce8e73c95fdf63ed8704b
Matteo Manna <matteomanna87@gmail.com>
2017-03-24 09:40:57 +0100

    Init CSS and JS
    Init CSS Style and javascript files. Plugin name changed.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: WP Lorem Ipsum
+Plugin Name: WP Lorem ipsum
 Description: Automatically create new fake posts to fill the database and get a very good impression for your website.
 Author: Matteo Manna
 Version: 1.0
@@ -8,3 +8,8 @@ Author URI: http://matteomanna.com/
 Text Domain: wp-lorem-ipsum
 License: GPL2
 */
+
+function li_load_textdomain() {
+    load_plugin_textdomain( 'wp-lorem-ipsum', false, basename( dirname( __FILE__ ) ).'/languages' );
+}
+add_action( 'init', 'li_load_textdomain' );
