221b02308fe7c0aed8a146339e73c337bcc72e05
Matteo Manna <matteomanna87@gmail.com>
2017-03-27 12:19:56 +0200

    Updated functions
    New functions introduced and updated old functions.

--- index.php
+++ index.php
@@ -9,6 +9,18 @@ Text Domain: wp-lorem-ipsum
 License: GPL2
 */
 
-require('class.posts.php');
-require('class.thumbnails.php');
-require('wp.functions.php');
+// Automatically require all library files
+$lib_path = dirname(__FILE__).'/lib/';
+if ( file_exists( $lib_path ) ) {
+    $open = opendir( $lib_path );
+    while( false !== ( $file = readdir( $open ) ) ) if ( $file != '.' && $file != '..' ) require('lib/'.$file);
+}
+
+/**
+ * @param $plugin
+ * @Usage: Redirect after Plugin activation
+ */
+function li_redirect_after_activation( $plugin ) {
+    if( $plugin == plugin_basename( __FILE__ ) ) exit( wp_redirect( admin_url('options-general.php?page=wp-lorem-ipsum') ) );
+}
+add_action( 'activated_plugin', 'li_redirect_after_activation' );
--- lib/class.posts.php
+++ lib/class.posts.php
@@ -9,25 +9,10 @@ if( !class_exists('LiPosts') ) {
 
         /**
          * @param string $key
-         * @return mixed
-         */
-        private function get_post_content( $key = 'post_title' ) {
-            //https://market.mashape.com/montanaflynn/lorem-text-generator
-            $array = array(
-                'post_title' => 'test di prova come titolo',
-                'post_content' => 'testo di prova come fosse Lorem Ipsum',
-                'post_excerpt' => 'excerpt di prova'
-            );
-            $return = ( isset($array[$key]) ) ? $array[$key] : '' ;
-
-            return $return;
-        }
-
-        /**
-         * @param string $key
          * @return int
          */
-        public function get_numbers_post_limit( $key = 'min' ) {
+        public function get_count_post_limit( $key = 'min' )
+        {
             $array = array(
                 'min' => 5,
                 'max' => 50
@@ -38,20 +23,25 @@ if( !class_exists('LiPosts') ) {
         }
 
         /**
-         * @param array $args
+         * @param array $args:
+         *      - @post_number: 5 <=> 50. Default 5
+         *      - @post_type: Default post
+         *      - @post_status: publish, future, draft, pending, private, trash, auto-draft, inherit. Default publish
+         *      - @post_author: Default current user ID
+         *      - @has_post_thumbnail: Default true
          * @return array
          */
-        public function wp_insert_post( $args = array() )
+        public function insert_posts( $args = array() )
         {
             // Posts number: find "min" and "max" from dedicated function. Default "min"
             $posts_number = (
                 isset($args['posts_number'])
                 && is_numeric($args['posts_number'])
                 && (
-                    $args['posts_number'] >= $this->get_numbers_post_limit('min')
-                    && $args['posts_number'] <= $this->get_numbers_post_limit('max')
+                    $args['posts_number'] >= $this->get_count_post_limit('min')
+                    && $args['posts_number'] <= $this->get_count_post_limit('max')
                 )
-            ) ? (int)$args['posts_number'] : $this->get_numbers_post_limit('min') ;
+            ) ? (int)$args['posts_number'] : $this->get_count_post_limit('min') ;
 
             // Post type, default post
             $post_type = ( isset($args['post_type']) && !empty($args['post_type']) ) ? sanitize_text_field($args['post_type']) : 'post' ;
@@ -62,7 +52,7 @@ if( !class_exists('LiPosts') ) {
             // Post author: default current user ID
             $post_author = ( isset($args['post_author']) && is_numeric($args['post_author']) && $args['post_author'] > 0 ) ? (int)$args['post_author'] : get_current_user_id() ;
 
-            // Has post thumbnail
+            // Has post thumbnail, default true
             $has_post_thumbnail = ( isset($args['has_post_thumbnail']) && is_bool($args['has_post_thumbnail']) ) ? $args['has_post_thumbnail'] : true ;
 
             // All new posts IDs, init array
@@ -72,16 +62,14 @@ if( !class_exists('LiPosts') ) {
                 $post = array(
                     'post_type' => $post_type,
                     'post_status' => $post_status,
-                    'post_title' => wp_strip_all_tags( $this->get_post_content('post_title') ),
-                    'post_content' => $this->get_post_content('post_content'),
-                    'post_excerpt' => $this->get_post_content('post_excerpt'),
+                    'post_title' => wp_strip_all_tags( li_get_rand_content('post_title') ),
+                    'post_content' => li_get_rand_content('post_content'),
+                    'post_excerpt' => li_get_rand_content('post_excerpt'),
                     'post_author' => (int)$post_author
                 );
                 $post_id = wp_insert_post( $post );
 
-                /**
-                 * If has post Thumbnails
-                 */
+                // If has post Thumbnails
                 if ( $has_post_thumbnail == true ) {
                     // Get Class LiThumbnails
                     global $li_thumbnails;
